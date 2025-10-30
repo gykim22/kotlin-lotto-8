a681b2600632874eb783b70dafc82f7dad5fc753
cloud-23k <k_bright_st@pusan.ac.kr>
2025-10-31 00:34:32 +0900

    feat(LottoView.kt): 로또 구입 개수 및 내역 출력 로직 작성

--- src/main/kotlin/lotto/view/LottoView.kt
+++ src/main/kotlin/lotto/view/LottoView.kt
@@ -1,6 +1,7 @@
 package lotto.view
 
 import camp.nextstep.edu.missionutils.Console
+import lotto.Lotto
 
 class LottoView {
     fun getMoney(): Int {
@@ -8,7 +9,12 @@ class LottoView {
         return Console.readLine().toInt()
     }
 
-    fun getLottoNumbers(): List<Int>{
+    fun printLottoList(lottoList: List<Lotto>) {
+        println("${lottoList.size}개를 구매했습니다.")
+        for (lotto in lottoList) { println(lotto) }
+    }
+
+    fun getLottoNumbers(): List<Int> {
         println("당첨 번호를 입력해 주세요.")
         return Console.readLine().split(",").map { it.toInt() }
     }
