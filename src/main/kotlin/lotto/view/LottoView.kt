0a894f9f764a364e4e72102fe969df5ec56306ef
cloud-23k <k_bright_st@pusan.ac.kr>
2025-10-30 23:52:26 +0900

    feat(LottoView.kt): 사용자 입력 로직 작성
    - getMoney : 금액 입력
    - getLottoNumbers : 당첨 번호 입력
    - getBonusNumber : 보너스 번호 입력

---
+++ src/main/kotlin/lotto/view/LottoView.kt
@@ -0,0 +1,20 @@
+package lotto.view
+
+import camp.nextstep.edu.missionutils.Console
+
+class LottoView {
+    fun getMoney(): Int {
+        println("구입금액을 입력해 주세요.")
+        return Console.readLine().toInt()
+    }
+
+    fun getLottoNumbers(): List<Int>{
+        println("당첨 번호를 입력해 주세요.")
+        return Console.readLine().split(",").map { it.toInt() }
+    }
+
+    fun getBonusNumber(): Int {
+        println("보너스 번호를 입력해 주세요.")
+        return Console.readLine().toInt()
+    }
+}
