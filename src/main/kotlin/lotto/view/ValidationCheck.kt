b7c3e771eb1c85133246bc0f3a079d376b6e5e1b
cloud-23k <k_bright_st@pusan.ac.kr>
2025-10-31 12:43:31 +0900

    feat(ValidationCheck.kt): 사용자 입력 구문 작성 및 예외처리 - isValidMoneyAmount : 금액 입력 - isValidLottoNumber : 당첨 번호 입력 - isValidBonusNumber : 보너스 번호 입력 - 공통 사항 : 입력 유효성 및 요구사항 충족 여부 확인

---
+++ src/main/kotlin/lotto/view/ValidationCheck.kt
@@ -0,0 +1,49 @@
+package lotto.view
+
+import lotto.Lotto
+
+class ValidationCheck {
+    fun isValidMoneyAmount(): Int {
+        while (true) {
+            try {
+                val money = LottoView().getMoney()
+                require(money % 1000 == 0) { "[Error] 구입 금액은 1,000원 단위여야 합니다." }
+                require(money >= 1000) { "[ERROR] 구입 금액은 1000원 이상 자연수여야 합니다." }
+                return money
+            } catch (e: NumberFormatException) {
+                println("[ERROR] 올바른 문자 형식으로 입력해주세요.")
+            } catch (e: IllegalArgumentException) {
+                println(e)
+            }
+        }
+    }
+
+    fun isValidLottoNumber(): Lotto {
+        while (true) {
+            try {
+                val lottoNumbers = LottoView().getLottoNumbers()
+                return Lotto(lottoNumbers)
+            } catch (e: NumberFormatException) {
+                println("[ERROR] 올바른 문자 형식으로 입력해주세요.")
+            } catch (e: IllegalArgumentException) {
+                println(e.message ?: "[ERROR] 잘못된 입력입니다.")
+            }
+        }
+    }
+
+    fun isValidBonusNumber(lotto: Lotto): Int {
+        while (true) {
+            try {
+                val bonusNumber = LottoView().getBonusNumber()
+                require(bonusNumber in 1..45) { "[ERROR] 로또 번호는 1부터 45 사이여야 합니다." }
+                require(bonusNumber !in lotto.getSortedNumbers()) { "[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다." }
+                return bonusNumber
+            } catch (e: NumberFormatException) {
+                println("[ERROR] 올바른 문자 형식으로 입력해주세요.")
+            } catch (e: IllegalArgumentException) {
+                println(e.message ?: "[ERROR] 잘못된 입력입니다.")
+            }
+        }
+
+    }
+}
