62b353f832c4aee4000f7a520cdf56b44c2e3484
cloud-23k <k_bright_st@pusan.ac.kr>
2025-11-01 16:23:59 +0900

    feat(ValidationCheck.kt): 사용자 입력 유효성 검증 로직 작성
    - isValidMoneyAmount : 사용자의 금액 입력값 검증
    - isValidLottoNumber : 당첨 로또 번호 입력값 검증
    - isValidBonusNumber : 보너스 번호 입력값 검증

--- src/main/kotlin/lotto/view/ValidationCheck.kt
+++ src/main/kotlin/lotto/view/ValidationCheck.kt
@@ -9,7 +9,7 @@ class ValidationCheck {
                 val money = LottoView().getMoney()
                 require(money % 1000 == 0) { "[Error] 구입 금액은 1,000원 단위여야 합니다." }
                 require(money >= 1000) { "[ERROR] 구입 금액은 1000원 이상 자연수여야 합니다." }
-                return money
+                return money / 1000
             } catch (e: NumberFormatException) {
                 println("[ERROR] 올바른 문자 형식으로 입력해주세요.")
             } catch (e: IllegalArgumentException) {
