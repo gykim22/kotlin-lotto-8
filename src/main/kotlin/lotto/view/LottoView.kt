6d79dc63183208a243e195e5c203c2eff7a644df
cloud-23k <k_bright_st@pusan.ac.kr>
2025-11-01 16:22:44 +0900

    feat(LottoView.kt): 콘솔 화면 출력을 위한 메소드 작성
    - getMoney : 사용자로부터 구입금액 입력
    - getLottoNumbers : 당첨 번호 입력
    - getBonusNumber : 보너스 번호 입력
    - printLottoList : 구매한 총 로또 출력
    - printWinningResult : 당첨 통계 출력
    - printProfitRate : 수익률 출력

--- src/main/kotlin/lotto/view/LottoView.kt
+++ src/main/kotlin/lotto/view/LottoView.kt
@@ -2,6 +2,7 @@ package lotto.view
 
 import camp.nextstep.edu.missionutils.Console
 import lotto.Lotto
+import lotto.model.LottoRank
 
 class LottoView {
     fun getMoney(): Int {
@@ -21,6 +22,30 @@ class LottoView {
 
     fun printLottoList(lottoList: List<Lotto>) {
         println("${lottoList.size}개를 구매했습니다.")
-        for (lotto in lottoList) { println(lotto) }
+        for (lotto in lottoList) {
+            println(lotto.getSortedNumbers())
+        }
+    }
+
+    fun printWinningResult(results: List<LottoRank>) {
+        println("당첨 통계\n---")
+        LottoRank.entries
+            .filter { it != LottoRank.LOSES }
+            .forEach { rank ->
+                val count = results.count { it == rank }
+                val label = when (rank) {
+                    LottoRank.FIRST -> "6개 일치"
+                    LottoRank.SECOND -> "5개 일치, 보너스 볼 일치"
+                    LottoRank.THIRD -> "5개 일치"
+                    LottoRank.FOURTH -> "4개 일치"
+                    else -> "3개 일치"
+                }
+                println("$label (${String.format("%,d", rank.prize)}원) - ${count}개")
+            }
+    }
+
+    fun printProfitRate(results: List<LottoRank>, money: Int) {
+        val totalProfitRate = results.sumOf { it.prize }
+        println("총 수익률은 %.1f".format(totalProfitRate.toDouble() / money * 100) + "%입니다.")
     }
 }
