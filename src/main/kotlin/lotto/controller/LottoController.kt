db4e30342057cd13a01ee003f1e1bc97388d1367
cloud-23k <k_bright_st@pusan.ac.kr>
2025-11-01 20:48:29 +0900

    refactor(LottoController.kt): 클래스 분리에 따른 컨트롤러 로직 수정

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -1,24 +1,61 @@
 package lotto.controller
 
+import lotto.Lotto
 import lotto.model.LottoGame
+import lotto.model.LottoParser
 import lotto.model.LottoRank
+import lotto.model.ValidationCheck
 import lotto.view.LottoView
-import lotto.view.ValidationCheck
 
-class LottoController {
+class LottoController(
+    private val view: LottoView = LottoView()
+) {
     fun run() {
-        val validatedAmount = ValidationCheck().isValidMoneyAmount()
-        val lottoNumbers = ValidationCheck().isValidLottoNumber()
-        val bonusNumber = ValidationCheck().isValidBonusNumber(lottoNumbers)
+        val validatedAmount = getValidateMoney()
+        val lottoNumbers = getValidateLottoNumbers()
+        val bonusNumber = getValidateBonusNumber(lottoNumbers)
         val lottoList = LottoGame.generateLotto(validatedAmount)
 
-        LottoView().printLottoList(lottoList)
+        view.printLottoList(lottoList)
         val results = lottoList.map {
             val matchCount = it.getSortedNumbers().count { num -> num in lottoNumbers.getSortedNumbers() }
             val bonusMatch = bonusNumber in it.getSortedNumbers()
             LottoRank.of(matchCount, bonusMatch)
         }
-        LottoView().printWinningResult(results)
-        LottoView().printProfitRate(results, validatedAmount * 1000)
+        view.printWinningResult(results)
+        view.printProfitRate(results, validatedAmount * 1000)
+    }
+
+    private fun getValidateMoney(): Int {
+        while (true) {
+            try {
+                val money = view.getMoney().let(LottoParser::money)
+                return ValidationCheck.isValidMoneyAmount(money)
+            } catch (e: IllegalArgumentException) {
+                println(e.message ?: "[ERROR] 잘못된 입력입니다.")
+            }
+        }
+    }
+
+    private fun getValidateLottoNumbers(): Lotto {
+        while (true) {
+            try {
+                val lottoNumbers = view.getLottoNumbers().let(LottoParser::lottoNumbers)
+                return Lotto.of(lottoNumbers)
+            } catch (e: IllegalArgumentException) {
+                println(e.message ?: "[ERROR] 잘못된 입력입니다.")
+            }
+        }
+    }
+
+    private fun getValidateBonusNumber(main: Lotto): Int {
+        while (true) {
+            try {
+                val bonusNumber = view.getBonusNumber().let(LottoParser::bonusNumber)
+                return ValidationCheck.isValidBonusNumber(bonusNumber, main.getSortedNumbers().toSet())
+            } catch (e: IllegalArgumentException) {
+                println(e.message ?: "[ERROR] 잘못된 입력입니다.")
+            }
+        }
     }
 }
